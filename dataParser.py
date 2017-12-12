fd81a71750b9615dd0e5fde0c26abd5fc1752084
Cameron Merrill <cmm2@illinois.edu>
2017-12-12 14:28:45 -0600

    euclidean distance WIP

--- dataParser.py
+++ dataParser.py
@@ -1,5 +1,5 @@
 from os import walk
-
+import math
 
 #Raw Data in Logs/*
 #We are converting the raw Data from the Wii Balance board (position data) into center of pressure total path length
@@ -22,8 +22,8 @@ files = filesPruned
 
 #for each file we need an I and O
 
-masterFile = open("master-cop.csv", 'w')
-masterFile.write("FileName, Total X, Total Y, Total D, Total T, Samples\n")
+masterFile = open("master-cop-eucl.csv", 'w')
+masterFile.write("FileName, Total X, Total Y, Total D, Total T, Distance, Samples\n")
 
 for item in files:
 	itemName = item.split(".")[0]
@@ -34,6 +34,7 @@ for item in files:
 	totalX = 0
 	totalY = 0
 	totalT = 0
+	totalD = 0
 	for line in inputFile:
 		lineSplit = line.split(" ")
 		lineX = float(lineSplit[0])
@@ -43,11 +44,14 @@ for item in files:
 			totalX = totalX + abs(lineX - prevX)
 			totalY = totalY + abs(lineY - prevY)
 			totalT = totalT + abs(lineT - prevT)
+			x_valu = abs(lineX - prevX)
+			y_valu = abs(lineY - prevY)
+			totalD = totalD + math.sqrt(math.pow(x_valu,2) + math.pow(y_valu,2))
 		prevX = float(lineSplit[0])
 		prevY = float(lineSplit[1])
 		prevT = float(lineSplit[2])
 		count = count + 1
 	
 	outputFile.write("X Pos, Y Pos, Time, dT, dX Pos, dY Pos,," + str(count) + "," + str(totalX) + "," + str(totalY) + "," + str(totalT) + "," + str(totalX+totalX))
-	masterFile.write(item.split(".")[0] + "," + str(totalX) + "," + str(totalY) + "," + str(totalX + totalY) + "," + str(totalT) + "," + str(count) + "\n")
+	masterFile.write(item.split(".")[0] + "," + str(totalX) + "," + str(totalY) + "," + str(totalX + totalY) + "," + str(totalT) + "," + str(totalD) + "," + str(count) + "\n")
 
