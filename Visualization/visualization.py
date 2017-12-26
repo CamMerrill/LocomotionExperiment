59598ac17ede2473e4dd4efdf9887c069e115f3f
Cameron Merrill <cmm2@illinois.edu>
2017-12-25 18:29:07 -0600

    wooooooords

--- Visualization/visualization.py
+++ Visualization/visualization.py
@@ -1,4 +1,60 @@
+from os import walk
+import math
+import matplotlib.pyplot as plt
+
+#Get All Logs, again
+
+files = []
+for (dirpath, dirnames, filenames) in walk('../Logs'):
+	files.extend(filenames)
+	break
+filesPruned = []
+for item in files:
+	splitItem = item.split(".")
+	if splitItem[1] == "csv":
+		filesPruned.append(item)
+
+files = filesPruned #lazy copy
+
+#Now we have a list of all csv files, lets prune
+maxX = 0
+maxY = 0
+previousSubject = 1
+for item in files:
+	inputFile = open("../Logs/" + item, 'r')
+	
+	#output file goes here, probably
+	#outputfile
+	xVals = []
+	yVals = []
+
+	for line in inputFile:
+		lineSplit = line.split(" ")
+		lineX = float(lineSplit[0])
+		lineY = float(lineSplit[1])
+		if abs(lineX) > maxX:
+			maxX = abs(lineX)
+		if abs(lineY) > maxY:
+			maxY = abs(lineY)
+
+		xVals.append(lineX)
+		yVals.append(lineY)
+
+		#NEED TO DRAW
 
 
+	currentSubject = item[0]
+	if currentSubject != previousSubject:
+		plt.savefig("Figures/Normalized/" + currentSubject + ".png")
+		plt.clf()
+		plt.figure(currentSubject)
+		count = 0
+	plt.subplot(count)
+	plt.plot(xVals, yVals, linewidth=0.5)
+	plt.title(item)
+	plt.axis([-200, 200, -125, 125])
+	previousSubject = currentSubject
+	#plt.show()
+	#plt.savefig("Figures/Normalized/" + item[:-4] + ".png")
 
-#To do, everything
+print (maxX, maxY)
